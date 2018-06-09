ee8af9159a71338d1807547fbb7e9ccc991a9e6a
Terminator <alexey.orlov2016@yandex.ru>
2018-06-09 21:17:59 +0400

    Socket and Connecting

--- server_others.py
+++ server_others.py
@@ -1,46 +1,83 @@
 # -*- coding: utf-8 -*-
 from getpass import getpass
+import threading
+
 import socket
 
 SOCKET = socket.socket()
-
-PASSWORT = "hackers"
-IP = None
-PORT = None
-LISTEN = None
-CONNECTS = 0
 
 #			   ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",]
 #   computer    1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17  18  19
 connect_list = []
 address_list = []
 
+PASSWORT = "hackers"
+IP = None
+PORT = None
+LISTEN = None
+CONNECTS = 0
+THREAD = None
+
 index = 0
 
+STATUS = 0
+# 0 - no SOCKET
+# 1 - created SOCKET, but no connectThread and no CONNECTS
+# 2 - created SOCKET and connectThread, but no  no CONNECTS
+# 3 - created SOCKET, connectThread, and have CONNECTS
+
+
 def printInfo():
+	s = None
+	if STATUS == 0:
+		s = "No SOCKET!"
+	elif STATUS == 1:
+		s = "SOCKET created | No connectThread | No CONNECTS"
+	elif STATUS == 2:
+		s = "SOCKET created | connectThread created | No CONNECTS"
+	elif STATUS == 3:
+		s = "SOCKET created | connectThread created | CONNECTS > 0"
+	else:
+		s = "ERROR!!!"
+
 	print()
 	print("________INFO________")
-	print("IP sever`s:"  +IP)
-	print("PORT sever`s:" + PORT)
-	print("LISTEN sever`s:" + LISTEN)
-	print("PASSWORT sever`s:" + PASSWORT)
-	print("CONNECTS sever`s:" + CONNECTS)
+	print("STATUS:", s)
+	print("IP sever`s:", IP)
+	print("PORT sever`s:", PORT)
+	print("LISTEN sever`s:", LISTEN)
+	print("PASSWORT sever`s:", PASSWORT)
+	print("CONNECTS sever`s:", CONNECTS)
 	print()
 
+def printHelp():
+	print()
+	print("________Commands________")
+	print("connect")
+	print("info")
+	print("help")
+	print("list")
+
+def printList():
+	print()
+	print("________List________")
+	for number in range(CONNECTS):
+		print(address_list[number])
 
 def login():
+	global PASSWORT
+
 	i = 0
 
 	while i < 4:
 		p = str(getpass("password: "))
 
-		if p == PASSWORD:	
+		if p == PASSWORT:	
 			return 1234
 		print("Incorrect password.")
-		i =+ 1
+		i += 1
 
 	print("Authorization completed unsuccessfully!")
-	#exit()
 
 
 def connected(i):
@@ -82,35 +119,94 @@ def computer():
 	else:
 		return "qwerty"
 
+class ConnectUsersThread(threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+		self.daemon = True
+
+	def connectingUsersThread():
+		global connect_list
+		global address_list
+		global CONNECTS
+		global STATUS
+
+		while True:
+			connect , address = SOCKET.accept()
+			connect_list.append(connect)
+			address_list.append(address)
+
+			CONNECTS += 1
+			STATUS = 3
+
+	def run(self):
+		global connect_list
+		global address_list
+		global CONNECTS
+		global STATUS
+
+		while True:
+			connect , address = SOCKET.accept()
+			connect_list.append(connect)
+			address_list.append(address)
+
+			CONNECTS += 1
+			STATUS = 3
+
+def connectCommand():
+	global THREAD
+	global STATUS
+
+	THREAD = ConnectUsersThread()
+	THREAD.start()
+
+	STATUS = 2
+
+def sendCommand():
+	print()
+	print("Connected" + CONNECTS + "users")
+
+	i = input("Enter number CONNECTS   (int or <all>)  : ")
+	connected(i)
+
+
 def interface():
 	while True:
-		command = input("Enter command: ")
+		try:
+			command = str(input("Enter command: "))
+		except Exception as EOFError:
+			pass
 
-		if command:
-			if command == 'exit':
-				break
-			elif command == 'connect':
-				p = input("Passwort: ")
-				if p == passwort:
-					i = input("Enter number connected   (int <= 19)  : ")
-					connected(int(i))
-			elif command == 'send':
-				p = input("Passwort: ")
-				if p == passwort:
-					data_get = computer()
-					if data_get != "qwerty":
-						ret(data_get.decode("utf-8"))
-			else:
-				print("Wir weisen diese <Command> nicht!")
-			print()
+		if command == 'exit':
+			break		
+		elif command == 'connect':
+			if login() == 1234:
+				connectCommand()
+		elif command == 'info':
+			printInfo()
+		elif command == 'help':
+			printHelp()
+		elif command == 'list':
+			printList()
+		elif command == 'send':
+			p = input("Passwort: ")
+			if p == passwort:
+				data_get = computer()
+				if data_get != "qwerty":
+					ret(data_get.decode("utf-8"))
 		elif not command:
 			print("You not enter command!")
 			continue
 		else:
-			print("Error!")
-			continue
+			print("Wir weisen diese <Command> nicht!")
+		print()
 
 def createSocket():
+	global IP
+	global PORT
+	global LISTEN
+	global SOCKET
+	global STATUS
+
 	print()
 	
 	IP = str(input("IP: "))
@@ -122,6 +218,8 @@ def createSocket():
 	SOCKET.bind((IP , PORT))
 	SOCKET.listen(LISTEN)
 
+	STATUS = 1
+
 	print("Socket created successfully!")
 	print()
 
@@ -131,6 +229,10 @@ def destroySocket():
 def main():
 	createSocket()
 	printInfo()
+
+	interface()
+
+	destroySocket()
 
 if __name__ == '__main__':
 	main()
