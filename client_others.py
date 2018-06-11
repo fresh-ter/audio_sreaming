8612a941b8e8136650c52abfe541c1ca6923974b
Terminator <alexey.orlov2016@yandex.ru>
2018-06-11 11:52:07 +0400

    Add all

--- client_others.py
+++ client_others.py
@@ -2,54 +2,34 @@
 import sys
 import random
 import socket
-
-color_list = {1 : "#ff0000",
-			  2 : "#008000",
-			  3 : "#483d8b",
-			  4 : "#6a5acd",
-			  5 : "#800080",
-			  6 : "#ffffff",
-			  7 : "#ffff00",
-			  8 : "#ff8c00",
-			  9 : "#ff60cb"}
+from os import system
 
 n = 0
 code = 0
 exit = 0
 passwort = "client"
 
-ip = '127.0.0.1' ############################################################################ /=)
-port = 9090      ############################################################################ \=)
+IP = None    	############################################################################ /=)
+PORT = 9090     ############################################################################ \=)
 
-ip = str(input("Enter IP server`s: "))
+IP = str(input("Enter IP server`s: "))
 
 
 def decode(msg):
 	return msg.decode("utf-8")
 
-
-def interpret(msg):
-	for d in ("color" , "volume"):
-		if d in msg and d == "color":
-			color_disp(msg)
-			return 0
-		elif d in msg and d == "volume":
-			volume_sys(msg)
-			return 0
-		elif msg == "exit":
-			exit = 1
-			return 0
-	return 1
-
-
-def color_disp(msg):
+def colorCommand(msg):
 	print("color_disp")
 	if "start" in msg:
 		print("Start color")
-		#=============================================== CMD =====================================
+		system("c:\\Python34\\python.exe c:\\audio_streaming\\background.py")
 	elif "none" in msg:
 		f = open(r"c:\audio_streaming\a.txt", "w", encoding="utf-8")
 		f.write("none")
+		f.close()
+	elif "exit" in msg:
+		f = open(r"c:\audio_streaming\a.txt", "w", encoding="utf-8")
+		f.write("exit")
 		f.close()
 	else:
 		for c in ('1','2','3','4','5','6','7','8','9'):
@@ -60,9 +40,25 @@ def color_disp(msg):
 				break
 
 
-def volume_sys(msg):
+def volumeCommand(msg):
 	print("volume_sys")
 
+def testCommand():
+	print("test")
+
+
+def commandMsg(msg):
+	for d in ("color" , "volume"):
+		if d in msg and d == "color":
+			colorCommand(msg)
+			return 3
+		elif d in msg and d == "volume":
+			volumeCommand(msg)
+			return 3
+		elif d in msg and d == 'test':
+			testCommand()
+			return 0
+	return 1
 
 
 
@@ -71,8 +67,8 @@ p = input("Passwort: ")
 
 if p == passwort:
 	
-	sock = socket.socket()
-	sock.connect((ip , port))
+	s = socket.socket()
+	s.connect((IP , PORT))
 	
 	print("Hello world!")
 
@@ -85,13 +81,19 @@ if p == passwort:
 			n += 1
 
 			if decode(data) == 'exit':
-				print(4321)
+				print("exit")
+
+				colorCommand("exit")
+
+				codeStr = "3"
+				sock.send(codeStr.encode("utf-8"))
 				break
-			code = interpret(decode(data))
-			ucode = str(code)
-			sock.send(ucode.encode("utf-8"))
+
+			code = commandMsg(decode(data))
+			codeStr = str(code)
+			sock.send(codeStr.encode("utf-8"))
 		print(1234)
 
-	sock.close()
+	s.close()
 
 sys.exit(app.exec_())
